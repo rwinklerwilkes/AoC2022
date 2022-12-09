59874e533b06ad6f4d12c6aabfdb55aceeaaaa0c
rwinklerwilkes <rich.w.sas@gmail.com>
2022-12-09 15:09:50 -0500

    Complete part 2

--- day9.py
+++ day9.py
@@ -49,6 +49,50 @@ class Board:
 
         self.distinct_positions.add(tuple(self.tail_pos))
 
+class Board_Part_Two:
+    def __init__(self):
+        self.head_pos = np.array([0,0])
+        self.knots = [np.array([0,0]) for i in range(9)]
+        self.distinct_positions = set()
+
+    def process_moves(self, data):
+        for inst in data.split('\n'):
+            self.move(inst)
+
+    def move(self, instruction):
+        direction, distance = instruction.split(' ')
+        distance = int(distance)
+        for i in range(distance):
+            match direction:
+                case 'U':
+                    self.head_pos[0] -= 1
+                case 'R':
+                    self.head_pos[1] += 1
+                case 'D':
+                    self.head_pos[0] += 1
+                case 'L':
+                    self.head_pos[1] -= 1
+            for i in range(9):
+                self.move_knot(i)
+
+    def move_knot(self, knot_to_move):
+        if knot_to_move == 0:
+            next_knot = self.head_pos
+        else:
+            next_knot = self.knots[knot_to_move-1]
+        knot = self.knots[knot_to_move]
+        difference = next_knot-knot
+        adjusted_diff = np.clip(difference, -1, 1)
+        distance = np.max(np.abs(difference))
+        #same position
+        if np.all(next_knot == knot) or distance <= 1:
+            #do nothing
+            pass
+        else:
+            self.knots[knot_to_move] += adjusted_diff
+
+        if knot_to_move == 8:
+            self.distinct_positions.add(tuple(self.knots[knot_to_move]))
 
 def part_one(data):
     b = Board()
@@ -56,6 +100,24 @@ def part_one(data):
     answer = len(b.distinct_positions)
     return answer
 
+def part_two(data):
+    b = Board_Part_Two()
+    b.process_moves(data)
+    answer = len(b.distinct_positions)
+    return answer
+
 test_part_one_answer = part_one(test_data)
 assert test_part_one_answer == 13
 part_one_answer = part_one(data)
+
+test_data_p2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+test_part_two_answer = part_two(test_data_p2)
+assert test_part_two_answer == 36
+part_two_answer = part_two(data)
