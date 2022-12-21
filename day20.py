f372a7e1e8249f9e264168e1150f74294ff50ef9
rwinklerwilkes <rich.w.sas@gmail.com>
2022-12-21 10:06:21 -0500

    Took longer than it should've because I overlooked instructions

--- day20.py
+++ day20.py
@@ -11,24 +11,62 @@ test_data = """1
 
 class Mixer:
     def __init__(self, data):
-        pd = self.parse_data(data)
+        pd, idxs = self.parse_data(data)
         self.data = pd
+        self.idxs = idxs
+        self.idx_to_use = 0
 
     def parse_data(self, data):
-        return [(i,int(val)) for i, val in enumerate(data.split('\n'))]
+        vals = [int(val) for i, val in enumerate(data.split('\n'))]
+        idxs = [i for i, val in enumerate(data.split('\n'))]
+        return vals, idxs
 
     def move_val(self, i):
-        orig_idx, val = self.data.pop(i)
+        val = self.data.pop(i)
+        idx = self.idxs.pop(i)
         new_pos = i + val
-        self.data.insert(new_pos%len(self.data), (orig_idx,val))
+        #Shifted off to the left
+        if new_pos == 0:
+            self.data.append(val)
+            self.idxs.append(idx)
+        else:
+            self.data.insert(new_pos % len(self.data), val)
+            self.idxs.insert(new_pos % len(self.idxs), idx)
         return self.data
 
     def move_orig_idx(self, i):
-        pass
+        cur_index = self.idxs.index(i)
+        self.move_val(cur_index)
 
     def part_one(self):
-        pass
+        answer = 0
+        for i in range(len(self.data)):
+            self.move_orig_idx(i)
+        zidx = self.data.index(0)
+        for i in range(1000,4000,1000):
+            answer += self.data[(zidx+i)%len(self.data)]
+        return answer
+
+    def part_two(self):
+        self.data = [i*811589153 for i in self.data]
+        answer = 0
+        for j in range(10):
+            for i in range(len(self.data)):
+                self.move_orig_idx(i)
+
+        zidx = self.data.index(0)
+        for i in range(1000,4000,1000):
+            answer += self.data[(zidx+i)%len(self.data)]
+        return answer
 
 m = Mixer(test_data)
+test_part_one_answer = m.part_one()
 
-m.move_val(0)
+m = Mixer(data)
+part_one_answer = m.part_one()
+
+m = Mixer(test_data)
+test_part_two_answer = m.part_two()
+
+m = Mixer(data)
+part_two_answer = m.part_two()
